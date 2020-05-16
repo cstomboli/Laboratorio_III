6d99a1813cf10b48c678ed4ab01e50ea6970179d
Carolina Stomboli <carostomboli@hotmail.com>
2020-05-16 17:07:01 -0300

    adelante un poco clase 5

--- Clase5/funciones.js
+++ Clase5/funciones.js
@@ -4,40 +4,39 @@ var http = new XMLHttpRequest;
                 var mostrar= document.getElementById("btnMostrar"); 
                 var container=this.document.getElementById("container");  
                 container.hidden=true; 
-                var btn = document.getElementById("btnEnviar");
-                btn.onclick = function(){
-                    var inputUser = document.getElementById("user").value;
-                    var inputPassword = document.getElementById("password").value;
+                var inputUser = document.getElementById("user").value;
+                var inputPassword = document.getElementById("password").value;
 
-                    http.onreadystatechange = callback;
-                    http.open("GET", "http://localhost:3000/loginUsuario?usr="+inputUser+"&pass="+inputPassword,true);
-                    /*  Si fuera post
-                    http.open("POST", "http://localhost:3000/loginUsuario",true);
-                    http.send("usr="+inputUser+"&pass="+inputPassword);
-                    */
-                    http.send();
-                   
-                    
-                }
+                http.onreadystatechange = callback;
+                http.open("GET", "http://localhost:3000/personas",true);
+                http.send();                 
+                
                 mostrar.onclick=function(){
-                    //alert("mues");
                     container.hidden=false;        
                 }
                 var cerrar=this.document.getElementById("btnCerrar");
                 cerrar.onclick=function(){
                     container.hidden=true;
-                } 
-                
+                }                 
             }
             function callback(){
-                if(http.readyState == 4 && http.status==200){
-                    if(http.response=="true")
-                    {
-                        container.hidden=true;
-                        var tabla = document.getElementById("tabla");
-                        var inputUser = document.getElementById("user").value;
-                        var inputPassword = document.getElementById("password").value;
-                        tabla.innerHTML += "<tr>"+"<td>"+inputUser+"</td><td>"+inputPassword+"</td><td><a href=''> Borrar</td>"+"</tr>";   
-                    }
+                if(http.readyState == 4 && http.status==200)
+                {
+                    armarGrilla(JSON.parse(http.responseText));    
                 }
-            } 
+                else{
+                    console.log("este error");
+                }
+            }       
+        function armarGrilla(jsonObj){
+            var tabla=document.getElementById("tabla");
+            for (var i=0; i<jsonObj.length; i++) //no va a tomar el length
+            {
+                var fila = "<tr><td>"+jsonObj[i].nombre+"</td>"+
+                "<td>"+jsonObj[i].apellido+"</td>"+
+                "<td>"+jsonObj[i].fecha+"</td>"+
+                "<td>"+jsonObj[i].telefono+"</td>"+"</tr>"
+                tabla.innerHTML=tabla.innerHTML+fila; 
+               // console.log(jsonObj[i].nombre);
+            }
+        }
