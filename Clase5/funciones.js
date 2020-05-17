c63a174f6e19e542c35438d2043dcd4e4e81940b
Carolina Stomboli <carostomboli@hotmail.com>
2020-05-17 16:58:06 -0300

    clase 5 terminada

--- Clase5/funciones.js
+++ Clase5/funciones.js
@@ -17,7 +17,21 @@ var http = new XMLHttpRequest;
                 var cerrar=this.document.getElementById("btnCerrar");
                 cerrar.onclick=function(){
                     container.hidden=true;
-                }                 
+                } 
+                
+                var guardar = document.getElementById("btnGuardar");
+                guardar.onclick=function(){
+                    var nombre = document.getElementById("user").value;
+                    var apellido = document.getElementById("password").value;
+                    var fecha = document.getElementById("fecha").value;
+                    var telefono = document.getElementById("telefono").value;
+
+                    var fila = "<tr><td>"+nombre+"</td>"+
+                        "<td>"+apellido+"</td>"+
+                        "<td>"+fecha+"</td>"+
+                        "<td>"+telefono+"</td>"+"</tr>"
+                        tabla.innerHTML=tabla.innerHTML+fila; 
+                }
             }
             function callback(){
                 if(http.readyState == 4 && http.status==200)
@@ -40,13 +54,5 @@ var http = new XMLHttpRequest;
                // console.log(jsonObj[i].nombre);
             }
         }
-
-        function nuevaPersona(){
-
-            
-            var fila = "<tr><td>"+jsonObj[i].nombre+"</td>"+
-                "<td>"+jsonObj[i].apellido+"</td>"+
-                "<td>"+jsonObj[i].fecha+"</td>"+
-                "<td>"+jsonObj[i].telefono+"</td>"+"</tr>"
-                tabla.innerHTML=tabla.innerHTML+fila; 
-        }
+        
+        
