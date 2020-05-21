5a5bce3d51bff1d3b839f25aaa36a3f9280b0ac9
Carolina Stomboli <carostomboli@hotmail.com>
2020-05-21 14:57:37 -0300

    casi terminado el modelo de parcial

--- modelo_pirmerParcial/PrimerModeloParcialLaboIII-master/funciones.js
+++ modelo_pirmerParcial/PrimerModeloParcialLaboIII-master/funciones.js
@@ -1,6 +1,7 @@
 var http = new XMLHttpRequest; 
 var idObtenido;
 var padre;
+var fechaActual = new Date();
 window.onload = function()
 {
     var mostrar= document.getElementById("btnMostrar"); 
@@ -37,6 +38,11 @@ window.onload = function()
         else if (document.getElementById("masculino").checked == true) 
         {
             var data = {nombre:document.getElementById("user").value,apellido:document.getElementById("password").value,fecha:document.getElementById("fecha").value,sexo:document.getElementById("masculino").value};
+            var fila = "<tr><td>"+document.getElementById("user").value+"</td>"+
+            "<td>"+document.getElementById("password").value+"</td>"+
+            "<td>"+document.getElementById("fecha").value+"</td>"+
+            "<td>"+document.getElementById("masculino").value+"</td>"+"</tr>"
+            tabla.innerHTML=tabla.innerHTML+fila; 
         } 
         http.onreadystatechange = function()
         {
@@ -58,67 +64,42 @@ window.onload = function()
         editar.onclick=function()
         {
             peticionPost("POST","http://localhost:3000/editar",true);
-           // alert(idObtenido);
-            
-            padre=padre.childNodes;
-                console.log(padre);
             hijo= padre.childNodes;
-            console.log(hijo);
+            console.log(hijo[1].textContent);
             hijo[0].textContent=idObtenido;
-            document.getElementById("user").value= hijo[1].textContent;
-            //document.getElementById("user").value= hijo[0].innerHTML; se puede escribir de las dos formas
-            document.getElementById("password").value=hijo[2].textContent;
-            document.getElementById("fecha").value =hijo [3].textContent;
-            if(hijo[4].textContent=='Female')
+            if(chequearDatosConAviso(document.getElementById("user").value, document.getElementById("password").value))
             {
-                document.getElementById("femenino").checked = true   
-            }
-            else if (hijo[3].textContent=='Male') 
-            {
-                document.getElementById("masculino").checked = true
-            } 
-
-            if(document.getElementById("femenino").checked == true)
-            {
-                
-                //var data = {nombre:document.getElementById("user").value,apellido:document.getElementById("password").value,fecha:document.getElementById("fecha").value,sexo:document.getElementById("femenino").value};
-                var fila = "<tr><td>"+document.getElementById("user").value+"</td>"+
-                "<td>"+document.getElementById("password").value+"</td>"+
-                "<td>"+document.getElementById("fecha").value+"</td>"+
-                "<td>"+document.getElementById("femenino").value+"</td>"+"</tr>"
-                tabla.innerHTML=tabla.innerHTML+fila; 
-            }
-            else if (document.getElementById("masculino").checked == true) 
-            {
-                //var data = {nombre:document.getElementById("user").value,apellido:document.getElementById("password").value,fecha:document.getElementById("fecha").value,sexo:document.getElementById("masculino").value};
+                hijo[1].textContent=document.getElementById("user").value;
+                //document.getElementById("user").value= hijo[0].innerHTML; se puede escribir de las dos formas
+                hijo[2].textContent=document.getElementById("password").value;
             }            
+            hijo [3].textContent=document.getElementById("fecha").value;
+            if(document.getElementById("femenino").checked = true)
+            {                
+                hijo[4].textContent=='Female'   
+            }
+            else if (document.getElementById("masculino").checked = true) 
+            {                
+                hijo[3].textContent=='Male'
+            }           
         }
 
-            var eliminar = document.getElementById("btnEliminar");
-            eliminar.onclick=function()
-            {
-                //removeChild
-                //una forma de hacer desaparecer, desde el padre al hijo
-                //var hijos = tabla.childNodes;
-                //de tal elemento ej. form.parentElement=null;
-                var nombre = document.getElementById("user").value;
-                var apellido = document.getElementById("password").value;
-                var fecha = document.getElementById("fecha").value;
-                //var sexo = document.getElementById("sexo").value;
-                if(document.getElementById("femenino").checked = true)
-                {
-                   var sexo = "Female";
-                }
-                else if (document.getElementById("masculino").checked = true) 
-                {
-                    var sexo = "Male";
-                } 
-
-                http.open("POST", "http://localhost:3000/eliminar",true);
-                http.send(); 
-
-            }
+        var eliminar = document.getElementById("btnEliminar");
+        eliminar.onclick=function()
+        {
+            peticionPostId("POST", "http://localhost:3000/eliminar", true, idObtenido)
+            console.log(idObtenido);
+            padre.remove();
+            //padre.removeChild(hijo[1]); //esto lo elimina, va borra ese contenido                
+        }
 }
+        function peticionPostId(metodo, url, funcion, id)
+        {
+            http.onreadystatechange =funcion;
+            http.open(metodo, url, true);
+            //http.setRequestHeader("Content-Type","application/json");                
+            http.send(id);
+        }
             
             function callback()
             {
@@ -176,11 +157,11 @@ window.onload = function()
             http.onreadystatechange =funcion;
             http.open(metodo, url, true);
             http.setRequestHeader("Content-Type","application/json");
-            if ((document.getElementById("femenino").checked==true) && document.getElementById("user").value.length>=3 && document.getElementById("password").value.length>=3)
+            if ((document.getElementById("femenino").checked==true) && (chequearDatos(document.getElementById("user").value, document.getElementById("password").value, document.getElementById("fecha").value)))
             {
                 var data = {id:idObtenido,nombre:document.getElementById("user").value,apellido:document.getElementById("password").value,fecha:document.getElementById("fecha").value,sexo:document.getElementById("femenino").value};
             }
-            else if ((document.getElementById("masculino").checked==true)&& document.getElementById("user").value.length>=3 && document.getElementById("password").value.length>=3)
+            else if ((document.getElementById("masculino").checked==true)&& (chequearDatos(document.getElementById("user").value, document.getElementById("password").value)))
             {
                 var data = {id:idObtenido,nombre:document.getElementById("user").value,apellido:document.getElementById("password").value,fecha:document.getElementById("fecha").value,sexo:document.getElementById("masculino").value};
             }
@@ -217,5 +198,52 @@ window.onload = function()
             {
                 document.getElementById("masculino").checked = true
             } 
+        }
+
+        function chequearDatos(nombre, apellido, fecha)
+        {
+            var retorno = true;
+            //var fechaActual= new Date();
+            //var fechaIngresada = Date(fecha);
+ 
+            if((nombre.value == "" || nombre.length<=3) || (apellido.value == "" || apellido.length<=3) )
+            {
+                if(nombre.value == "" || nombre.length<=3)
+                {
+                    document.getElementById("user").className="classError";
+                }                
+                else if(apellido.value == "" || apellido.length<=3)
+                {
+                    document.getElementById("password").className="classError";
+                }
+                /*
+                else if(fechaIngresada.getTime()>fechaActual.getTime())
+                {
+                    alert("la fecha debe ser anterior a la actual");
+                }    */
+                retorno = false;            
+            }          
+            return retorno;
+        }
+
+        function chequearDatosConAviso(nombre, apellido)
+        {
+            var retorno = true;
+
+            if((nombre.value == "" || nombre.length<=3) || (apellido.value == "" || apellido.length<=3))
+            {
+                if(nombre.value == "" || nombre.length<=3)
+                {
+                    document.getElementById("user").className="classError";
+                    alert("Debe ingresar nombre con mas de 3 letras");
+                }                
+                else if(apellido.value == "" || apellido.length<=3)
+                {
+                    document.getElementById("password").className="classError";
+                    alert("Debe ingresar apellido con mas de 3 letras");
+                }    
+                retorno = false;            
+            } 
+            return retorno;
         }
 
